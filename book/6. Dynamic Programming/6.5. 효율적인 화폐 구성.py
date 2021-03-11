95be01fe40540288a380e4b9cf0ff979ad7afc94
Soohee410 <ggohee0410@gmail.com>
2021-03-11 23:10:28 +0900

    .

---
+++ book/6. Dynamic Programming/6.5. 효율적인 화폐 구성.py
@@ -0,0 +1,15 @@
+N,M = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
+
+d = [10001]*(M+1)  # DP table
+d[0] = 0
+
+# Bottom-up
+for i in range(N):
+    for j in range(arr[i], M+1):
+        d[j] = min(d[j], d[j-arr[i]]+1)
+
+if d[M] == 10001:
+    print(-1)
+else:
+    print(d[M])
