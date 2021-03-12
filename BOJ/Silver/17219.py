09231ef9bf9ac30ce5abf20162293874a15b3302
Soohee410 <ggohee0410@gmail.com>
2021-03-12 23:29:55 +0900

    .

---
+++ BOJ/Silver/17219.py
@@ -0,0 +1,12 @@
+from sys import stdin, stdout
+
+N,M = map(int, stdin.readline().split())
+
+data = dict()
+for _ in range(N):
+  k, v = stdin.readline().split()
+  data[k] = v
+
+result = [data[stdin.readline().rstrip()] for _ in range(M)]
+
+stdout.write("\n".join(result))
